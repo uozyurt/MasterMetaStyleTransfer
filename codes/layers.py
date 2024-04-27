c786d00c0f5d6e830f9b742bd8fb4b5ac4f66317
Melih Gokay Yigit <melih.gokay8@icloud.com>
2024-04-27 15:11:01 +0300

    add MLP block

--- codes/layers.py
+++ codes/layers.py
@@ -4,6 +4,26 @@ import torch.nn.functional as F
 from timm.models.layers import trunc_normal_
 
 
+
+class Mlp(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
 class WindowAttention(nn.Module):
     r""" Window based multi-head self attention (W-MSA) module with relative position bias.
     It supports both of shifted and non-shifted window. This setup allows different updates for 
