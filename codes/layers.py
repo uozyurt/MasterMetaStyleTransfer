76683d495723b8761247d34bb8edb62abc8fc7fc
Melih Gokay Yigit <melih.gokay8@icloud.com>
2024-04-27 15:15:29 +0300

    add window_partition

--- codes/layers.py
+++ codes/layers.py
@@ -24,6 +24,21 @@ class Mlp(nn.Module):
         return x
 
 
+def window_partition(x, window_size):
+    """
+    Args:
+        x: (B, H, W, C)
+        window_size (int): window size
+
+    Returns:
+        windows: (num_windows*B, window_size, window_size, C)
+    """
+    B, H, W, C = x.shape
+    x = x.view(B, H // window_size, window_size, W // window_size, window_size, C)
+    windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, window_size, window_size, C)
+    return windows
+
+
 class WindowAttention(nn.Module):
     r""" Window based multi-head self attention (W-MSA) module with relative position bias.
     It supports both of shifted and non-shifted window. This setup allows different updates for 
