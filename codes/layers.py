c4a9b7ff1a3c27ee371100874268664c3f40377c
Melih Gokay Yigit <melih.gokay8@icloud.com>
2024-04-27 15:17:19 +0300

    update MLP activation

--- codes/layers.py
+++ codes/layers.py
@@ -6,7 +6,7 @@ from timm.models.layers import trunc_normal_
 
 
 class Mlp(nn.Module):
-    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.ReLU, drop=0.):
         super().__init__()
         out_features = out_features or in_features
         hidden_features = hidden_features or in_features
