14810c4dfacc7edd35ff068a3305077441280e8b
ozy5 <umutozyurt1907@hotmail.com>
2024-05-22 20:47:24 +0300

    added swin_T, swin_S support

--- codes/utils.py
+++ codes/utils.py
@@ -53,7 +53,8 @@ def download_VGG19_and_create_cutted_model_to_process(absolute_project_path,
 
 
 def download_swin_and_create_cutted_model(absolute_project_path,
-                                          model_save_relative_path="weights/swin_B_first_2_stages.pt"):
+                                          model_save_relative_path=None,
+                                          swin_variant="swin_B"):
         """
         Loads the Swin Transformer model from torchvision and saves the model with the first 2 stages.
 
@@ -62,13 +63,27 @@ def download_swin_and_create_cutted_model(absolute_project_path,
         Load code: <https://pytorch.org/vision/main/models/swin_transformer.html>
         """
 
+        # check if the swin variant is valid
+        if(swin_variant not in ["swin_T", "swin_S", "swin_B"]):
+                raise ValueError("Invalid Swin Transformer variant. Please choose one of the following: swin_T, swin_S, swin_B")
+
+        # if model save path is not given, set it to default
+        if(model_save_relative_path is None):
+                model_save_relative_path = os.path.join("models", f"{swin_variant}_first_2_stages.pt")
+
         # get the absolute path of the model save path
         model_save_absolute_path = os.path.join(absolute_project_path, model_save_relative_path)
 
         # if the model is not already saved, download the model and save it
         if not os.path.exists(model_save_absolute_path):
-                # get the swin transformer base model (download the weights from the internet if not already downloaded)
-                swin_transformer_base = swin_transformer.swin_b(weights="IMAGENET1K_V1")
+
+                # get the swin transformer model (download the weights from the internet if not already downloaded)
+                if(swin_variant == "swin_T"):
+                        swin_transformer_base = swin_transformer.swin_t(weights=swin_transformer.Swin_T_Weights.IMAGENET1K_V1)
+                elif(swin_variant == "swin_S"):
+                        swin_transformer_base = swin_transformer.swin_s(weights=swin_transformer.Swin_S_Weights.IMAGENET1K_V1)
+                elif(swin_variant == "swin_B"):
+                        swin_transformer_base = swin_transformer.swin_b(weights=swin_transformer.Swin_B_Weights.IMAGENET1K_V1)
 
                 # get the model features from 0 to 4, which correstpondes to the first 2 stages (before 2. patch merging)
                 swin_B_first_2_stages = Sequential(*list(swin_transformer_base.features)[:4])
